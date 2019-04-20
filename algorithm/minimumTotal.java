75250fe31d518da50fda91f9405768ab7a5636f5
hackjus <1096024838@qq.com>
2019-04-20 22:55:07 +0800

    day

--- algorithm/minimumTotal.java
+++ algorithm/minimumTotal.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 import java.util.List;
 
+import com.sun.management.jmx.Trace;
+
 public class minimumTotal {
 	int minSum=Integer.MAX_VALUE;
     public int mintotal(List<List<Integer>> triangle) {
@@ -25,7 +27,20 @@ public class minimumTotal {
     	_dfs(triangle, i+1, j+1,path,sum);
     	return 0;
     }
-
+    public int mintotal2(List<List<Integer>> triangle) {
+    	int minSum;
+    if(triangle.size()==0|| triangle.get(0).size()==0) {	
+    	return 0;
+    }
+    for(int i=triangle.size()-2;i>=0;--i) {
+    	for(int j=triangle.get(i).size()-1;j>=0;--j) {
+    		int min=Math.min(triangle.get(i+1).get(j), triangle.get(i+1).get(j+1));
+    		min+=triangle.get(i).get(j);
+    		triangle.get(i).set(j, min);
+    	}
+    }
+    return triangle.get(0).get(0);
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         minimumTotal oneMinimumTotal=new minimumTotal();
